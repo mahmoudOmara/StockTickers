35d37ed51bff9c037475e6dcd7f1d7e920e5e188
Mahmoud Omara <mahmoud.omara909@gmail.com>
2022-04-28 23:16:42 +0200

    Add NewsFeedService and NewsFeedServiceAPI

---
+++ StockTickers/NewsFeedService.swift
@@ -0,0 +1,30 @@
+//
+//  File.swift
+//  StockTickers
+//
+//  Created by mac on 28/04/2022.
+//
+
+import Foundation
+import Combine
+import Alamofire
+
+protocol NewsFeedService {
+    func fetchNewsFeed() -> AnyPublisher<DataResponse<NewsFeed, AFError>, Never>
+}
+
+class NewsFeedAPI: NewsFeedService {
+    let url: URL
+    
+    init(url: URL) {
+        self.url = url
+    }
+    
+    func fetchNewsFeed() -> AnyPublisher<DataResponse<NewsFeed, AFError>, Never> {
+        return AF.request(url, method: .get)
+            .validate()
+            .publishDecodable(type: NewsFeed.self)
+            .receive(on: DispatchQueue.main)
+            .eraseToAnyPublisher()
+    }
+}
