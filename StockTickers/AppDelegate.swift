404617b56ec4b42e6b4f838692d001279eb7da16
Mahmoud Omara <mahmoud.omara909@gmail.com>
2022-04-28 21:54:25 +0200

    add NewsFeedViewController and make it the entry point of the app

--- StockTickers/AppDelegate.swift
+++ StockTickers/AppDelegate.swift
@@ -9,11 +9,20 @@ import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
     var window: UIWindow?
-
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        
+        
+        let initialViewController = NewsFeedViewController()
+        
+        let navigationController = UINavigationController(rootViewController: initialViewController)
+        
+        window?.rootViewController = navigationController
+        window?.makeKeyAndVisible()
         
         return true
     }
---
+++ StockTickers/NewsFeedViewController.swift
@@ -0,0 +1,19 @@
+//
+//  NewsFeedViewController.swift
+//  StockTickers
+//
+//  Created by mac on 28/04/2022.
+//
+
+import UIKit
+
+class NewsFeedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+    }
+
+}
