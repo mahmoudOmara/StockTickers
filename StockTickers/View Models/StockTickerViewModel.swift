ece18cc19d4e95220edc47df2d1070050c9eadd2
Mahmoud Omara <mahmoud.omara909@gmail.com>
2022-04-29 12:58:56 +0200

    add StockTickerViewModel

---
+++ StockTickers/View Models/StockTickerViewModel.swift
@@ -0,0 +1,17 @@
+//
+//  StockTickerViewModel.swift
+//  StockTickers
+//
+//  Created by mac on 29/04/2022.
+//
+
+import Foundation
+
+class StockTickerViewModel {
+    
+    private let stockTicker: StockTicker
+    
+    init(stockTicker: StockTicker) {
+        self.stockTicker = stockTicker
+    }
+}
