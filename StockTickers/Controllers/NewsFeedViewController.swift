c59b2469fffcc9f31ab43c28b60cbcc0e1337b3c
Mahmoud Omara <mahmoud.omara909@gmail.com>
2022-04-29 18:03:48 +0200

    add typealiases for DataSource and Snapshot and implementation of makeDataSource func that return DataSource telling how the collectionView will fill its cells and headers

--- StockTickers/Controllers/NewsFeedViewController.swift
+++ StockTickers/Controllers/NewsFeedViewController.swift
@@ -9,10 +9,6 @@ import UIKit
 
 class NewsFeedViewController: UIViewController {
 
-    private var collectionView: UICollectionView!
-
-    private var viewModel: NewsFeedViewModel?
-    
     enum Section: String {
       case stockTickers = "Stocks"
       case latestNews = "Latest News"
@@ -24,8 +20,14 @@ class NewsFeedViewController: UIViewController {
         case latestNews(NewsItemViewModel)
         case remainigNews(NewsItemViewModel)
     }
+    
+    typealias DataSource = UICollectionViewDiffableDataSource<Section, Item>
+    typealias Snapshot = NSDiffableDataSourceSnapshot<Section, Item>
+    
+    private var collectionView: UICollectionView!
+    private var viewModel: NewsFeedViewModel?
+    private lazy var dataSource = makeDataSource()
 
-    var dataSource: UICollectionViewDiffableDataSource<Section, Item>! = nil
     
     convenience init(viewModel: NewsFeedViewModel) {
         self.init()
@@ -77,5 +79,39 @@ extension NewsFeedViewController {
     
     private func generateRemainingLayout() -> NSCollectionLayoutSection {
         return NSCollectionLayoutSection(group: NSCollectionLayoutGroup(layoutSize: NSCollectionLayoutSize(widthDimension: .absolute(10), heightDimension: .absolute(10))))
+    }
+    
+    func makeDataSource() -> DataSource {
+        let dataSource = DataSource(collectionView: collectionView) { collectionView, indexPath, itemIdentifier in
+            switch itemIdentifier {
+            case .stockTicker(let viewModel):
+                let cell = collectionView.dequeueReusableCell(withReuseIdentifier: StockTickerCollectionViewCell.reuseIdentifier, for: indexPath) as! StockTickerCollectionViewCell
+                cell.configure(with: viewModel)
+                return cell
+            case .latestNews(let viewModel):
+                let cell = collectionView.dequeueReusableCell(withReuseIdentifier: LatestNewsCollectionViewCell.reuseIdentifier, for: indexPath) as! LatestNewsCollectionViewCell
+                cell.configure(with: viewModel)
+                return cell
+            case .remainigNews(let viewModel):
+                let cell = collectionView.dequeueReusableCell(withReuseIdentifier: NewsCollectionViewCell.reuseIdentifier, for: indexPath) as! NewsCollectionViewCell
+                cell.configure(with: viewModel)
+                return cell
+            }
+        }
+        
+        dataSource.supplementaryViewProvider = { collectionView, kind, indexPath in
+            guard kind == HeaderViewCollectionReusableView.sectionHeaderElementKind else {
+                return nil
+            }
+            let section = self.dataSource.snapshot()
+                .sectionIdentifiers[indexPath.section]
+            let view = collectionView.dequeueReusableSupplementaryView(
+                ofKind: kind,
+                withReuseIdentifier: HeaderViewCollectionReusableView.reuseIdentifier,
+                for: indexPath) as? HeaderViewCollectionReusableView
+            view?.configure(with: section.rawValue)
+            return view
+        }
+        return dataSource
     }
 }
