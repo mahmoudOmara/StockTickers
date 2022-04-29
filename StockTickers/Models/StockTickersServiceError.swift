e10530d171404e57485089f70f0b8361e9153046
Mahmoud Omara <mahmoud.omara909@gmail.com>
2022-04-29 13:18:03 +0200

    add description to StockTickersServiceError

--- StockTickers/Models/StockTickersServiceError.swift
+++ StockTickers/Models/StockTickersServiceError.swift
@@ -11,3 +11,14 @@ enum StockTickersServiceError: Error {
     case CSVReadError(message: String)
     case others(message: String)
 }
+
+extension StockTickersServiceError: CustomStringConvertible {
+    public var description: String {
+        switch self {
+        case .CSVReadError(message: let message):
+            return message
+        case .others(message: let message):
+            return message
+        }
+    }
+}
