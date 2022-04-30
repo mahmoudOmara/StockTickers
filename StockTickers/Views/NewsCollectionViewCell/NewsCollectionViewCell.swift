c0e32222081f3777011807f4946a8d5742eaa639
Mahmoud Omara <mahmoud.omara909@gmail.com>
2022-04-30 04:26:42 +0200

    bind news's title, description, image and date to NewsCollectionViewCell views

--- StockTickers/Views/NewsCollectionViewCell/NewsCollectionViewCell.swift
+++ StockTickers/Views/NewsCollectionViewCell/NewsCollectionViewCell.swift
@@ -6,12 +6,47 @@
 //
 
 import UIKit
+import Combine
 
 class NewsCollectionViewCell: UICollectionViewCell {
-    private var viewModel: NewsItemViewModel?
     
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var newsImageView: UIImageView!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    
+    private var viewModel: NewsItemViewModel?
+    private var cancellableSet: Set<AnyCancellable> = []
+
     func configure(with viewModel: NewsItemViewModel) {
         self.viewModel = viewModel
+        viewModel
+            .$title
+            .sink { [weak self] in
+                self?.titleLabel.text = $0
+            }
+            .store(in: &cancellableSet)
+        
+        viewModel
+            .$image
+            .sink { [weak self] in
+                self?.newsImageView.image = $0
+            }
+            .store(in: &cancellableSet)
+        
+        viewModel
+            .$description
+            .sink { [weak self] in
+                self?.descriptionLabel.text = $0
+            }
+            .store(in: &cancellableSet)
+        
+        viewModel
+            .$date
+            .sink { [weak self] in
+                self?.dateLabel.text = $0
+            }
+            .store(in: &cancellableSet)
     }
 }
 
